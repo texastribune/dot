9db26875429300d84f1289b17a6273d0812567ff
AndrewGibson27 <atgibson27@gmail.com>
2020-05-20 17:01:45 -0500

    lowercase the table name

--- server/migrations/2-delete-visits.ts
+++ server/migrations/2-delete-visits.ts
@@ -2,6 +2,6 @@ import { QueryInterface } from 'sequelize';
 
 export default {
   up: (queryInterface: QueryInterface): Promise<void> => {
-    return queryInterface.dropTable('Visit');
+    return queryInterface.dropTable('visit');
   },
 };
