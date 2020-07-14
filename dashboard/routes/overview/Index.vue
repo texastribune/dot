00d526b1deeb162c688b342f7350112688076591
AndrewGibson27 <atgibson27@gmail.com>
2020-07-14 16:58:33 -0600

    move log-in redirect logic to parent route

--- dashboard/routes/overview/Index.vue
+++ dashboard/routes/overview/Index.vue
@@ -1,35 +1,8 @@
 <script lang="ts">
 import Vue from 'vue';
-import { mapGetters } from 'vuex';
-import { Route } from 'vue-router';
-
-import { NotAllowedError } from '../../errors';
-import { USER_MODULE } from '../../store';
-import { RouteMeta } from '../../types';
-import { logIn } from '../../auth';
 
 export default Vue.extend({
   name: 'OverviewRoute',
-
-  computed: {
-    ...mapGetters(USER_MODULE, ['isLoggedIn', 'isAllowed', 'userError']),
-  },
-
-  mounted(): void {
-    const route = this.$route as Route;
-    const {
-      requiresLogIn,
-      permissions: routePermissions,
-    } = route.meta as RouteMeta;
-
-    if (requiresLogIn && this.userError) {
-      throw this.userError;
-    } else if (requiresLogIn && !this.isLoggedIn) {
-      logIn();
-    } else if (!this.isAllowed(routePermissions)) {
-      throw new NotAllowedError();
-    }
-  },
 });
 </script>
 
