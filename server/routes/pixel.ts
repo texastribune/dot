433b756b278b7e8c211814c312297076ae57d8ac
AndrewGibson27 <atgibson27@gmail.com>
2020-12-01 12:12:56 -0600

    remove election-specific logic

--- server/routes/pixel.ts
+++ server/routes/pixel.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 
-import { TRACKER_JWT_SECRET, VALID_TRACKER_SOURCE } from '../config';
+import { TRACKER_JWT_SECRET } from '../config';
 import { TrackerTokenPayload } from '../types';
 import reportError from '../utils/report-error';
 import logError from '../utils/log-error';
@@ -17,48 +17,34 @@ const gif = Buffer.from(
 router.use('/pixel.gif', noCacheMiddleware);
 
 router.get('/pixel.gif', async (req, res) => {
-  const { token, contest, domain } = req.query;
+  const { token, domain } = req.query;
 
   try {
-    if (contest && typeof contest === 'string') {
-      const view = await View.create({
-        canonical: `https://apps.texastribune.org/features/2020/general-election-results/embeds/partner/?${contest}`,
-        source: VALID_TRACKER_SOURCE.DataViz,
-        domain,
-      });
-
-      await view.save();
-      // eslint-disable-next-line no-console
-      console.log(
-        `Logged view | Canonical: ${view.canonical} | Domain: ${view.domain} | Source: ${view.source}`
-      );
-    } else if (token && typeof token === 'string') {
-      const tokenPayload = await new Promise((resolve, reject) => {
-        jwt.verify(
-          token as string,
-          TRACKER_JWT_SECRET as string,
-          {
-            algorithms: ['HS256'],
-            ignoreExpiration: true,
-          },
-          (error, payload) => {
-            if (error) {
-              reject(error);
-            } else {
-              resolve(payload);
-            }
+    const tokenPayload = await new Promise((resolve, reject) => {
+      jwt.verify(
+        token as string,
+        TRACKER_JWT_SECRET as string,
+        {
+          algorithms: ['HS256'],
+          ignoreExpiration: true,
+        },
+        (error, payload) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(payload);
           }
-        );
-      });
-
-      const { canonical, source } = tokenPayload as TrackerTokenPayload;
-      const view = await View.create({ canonical, source, domain });
-
-      // eslint-disable-next-line no-console
-      console.log(
-        `Logged view | Canonical: ${view.canonical} | Domain: ${view.domain} | Source: ${view.source}`
+        }
       );
-    }
+    });
+
+    const { canonical, source } = tokenPayload as TrackerTokenPayload;
+    const view = await View.create({ canonical, source, domain });
+
+    // eslint-disable-next-line no-console
+    console.log(
+      `Logged view | Canonical: ${view.canonical} | Domain: ${view.domain} | Source: ${view.source}`
+    );
   } catch (error) {
     logError(error);
     reportError(error);
