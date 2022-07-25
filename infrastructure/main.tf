bb4e0f088fb44e9d6caf60bc2556c5554d6edc9a
Brandyn Friedly <bfriedly@texastribune.org>
2022-07-25 17:28:09 -0500

    add initial terraform config to configure google project
    This commit:
    - scaffolds a main.tf file to manage infrastructure for dot
    - declares which variables are needed to run this terraform file
    - configures terraform backend with terraform cloud
    - configures the google cloud provider (requires GOOGLE_CREDENTIALS environment file to be set in Terraform Cloud)
    - declares which Google APIs need to be enabled for the google project.  Currently requires IAM for permissions/credentials, artifact registry for hosting the dot container image, and cloud run.
    
    The Google project services declaration follows the example in Terraform in Action, by Scott Winkler (pg.164).

---
+++ infrastructure/main.tf
@@ -0,0 +1,49 @@
+# VARIABLES
+variable "GOOGLE_ORGANIZATION_ID" {}
+variable "GOOGLE_PROJECT_ID" {}
+variable "GOOGLE_REGION" { default = "us-central1" }
+
+# GLOBAL
+terraform {
+  required_providers {
+    # https://registry.terraform.io/providers/hashicorp/google/latest
+    google = {
+      source  = "hashicorp/google"
+      version = "4.20.0"
+    }
+  }
+  # Configure terraform cloud for terraform backend/statefile
+  cloud {
+    organization = "texastribune"
+    workspaces {
+      name = "dot"
+    }
+  }
+}
+
+# PROVIDERS
+# Requires GOOGLE_CREDENTIALS environment variable set at terraform runtime
+# See:  https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference
+provider "google" {
+  project = var.GOOGLE_PROJECT_ID
+  region  = var.GOOGLE_REGION
+}
+
+## DATA
+### local data
+locals {
+  # google project api services to enable
+  services = [
+    "iam.googleapis.com",
+    "artifactregistry.googleapis.com",
+    "run.googleapis.com",
+  ]
+}
+
+## RESOURCES
+### Enable/configure the needed Google Project api services
+resource "google_project_service" "enabled_service" {
+  for_each = toset(local.services)
+  project  = var.GOOGLE_PROJECT_ID
+  service  = each.key
+}
