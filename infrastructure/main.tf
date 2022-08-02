9f0da49ecf403bd69902d7ec4c19e970cbf13ab4
Brandyn Friedly <bfriedly@texastribune.org>
2022-08-02 10:30:57 -0500

    Remove google cloud provider and resources; configure heroku provider
    After some testing, we decided/found that Google Cloud Run wasn't a great
    fit for the way dot 3.0 flask app works challenges:
    1. Script assumes one running instance/unsure about race conditions with multiple containers/instances
    2. Script relies on system memory, Run removes containers and we can't assume the memory will exist with the request count
    3. The scheduler background process isn't a good fit for the request-based nature of Run.
    
    Because of these challenges, the script would need to be refactored to work with Cloud Run.
    Ideas included using a GCP memory service that each of the request-based containers could use,
    to deal with the count being in memory issue.  Secondly, having a Scheduled task service
    run the "flush" task at whatever interval against a common memorystore service could help
    with the issue of not having available cpu after a request ends/container is terminated.
    
    For the short term, for staging, we're switching to heroku to test the pipeline while
    the production plan develops.  Options include refactoring and trying cloud run again,
    deploying to a GKE cluster or virtual machine instance.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,18 +4,18 @@ variable "GOOGLE_PROJECT_ID" {}
 variable "GOOGLE_PROJECT_NAME" {}
 variable "GOOGLE_REGION" { default = "us-central1" }
 variable "SERVICE_NAME" {
-  type = string
+  type        = string
   description = "The name of the Cloud Run service/app"
-  default = "dot"
+  default     = "dot"
 }
 
 # GLOBAL
 terraform {
   required_providers {
-    # https://registry.terraform.io/providers/hashicorp/google/latest
-    google = {
-      source  = "hashicorp/google"
-      version = "4.30.0"
+    # https://registry.terraform.io/providers/heroku/heroku/latest
+    heroku = {
+      source  = "heroku/heroku"
+      version = "~> 5.1.1"
     }
   }
   # Configure terraform cloud for terraform backend/statefile
@@ -28,55 +28,13 @@ terraform {
 }
 
 # PROVIDERS
-# Requires GOOGLE_CREDENTIALS environment variable set at terraform runtime
-# See:  https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference
-provider "google" {
-  project = var.GOOGLE_PROJECT_ID
-  region  = var.GOOGLE_REGION
+provider "heroku" {
+  email   = var.heroku_email
+  api_key = var.heroku_api_key
 }
 
 ## DATA
 ### local data
-locals {
-  # google project api services to enable
-  services = [
-    "iam.googleapis.com",
-    "artifactregistry.googleapis.com",
-    "run.googleapis.com",
-  ]
-}
 
 ## RESOURCES
-### Enable/configure the needed Google Project api services
-resource "google_project_service" "enabled_service" {
-  for_each = toset(local.services)
-  project  = var.GOOGLE_PROJECT_ID
-  service  = each.key
-}
-
-module "cloud_run" {
-  # https://registry.terraform.io/modules/GoogleCloudPlatform/cloud-run/google/0.3.0
-  source = "GoogleCloudPlatform/cloud-run/google"
-  version = "~> 0.3.0"
-
-  service_name = var.SERVICE_NAME
-  project_id = var.GOOGLE_PROJECT_ID
-  location = var.GOOGLE_REGION
-  image = "us.gcr.io/${var.GOOGLE_PROJECT_NAME}/${var.SERVICE_NAME}:latest"
-  ports = {
-    "name": "http1",
-    "port": 5000
-  }
-  
-  # allow unauthenticated requests (makes service URL public)
-  members = ["allUsers"]
-
-}
-
 # OUTPUTS
-output "cloud_run-url" {
-  value = "${module.cloud_run.service_url}"
-}
-output "cloud_run-service-name" {
-  value = "${module.cloud_run.service_name}"
-}
