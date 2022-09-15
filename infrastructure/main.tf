5c14cb4b8ee205e65735efa5582e32780d5453f9
Brandyn Friedly <bfriedly@texastribune.org>
2022-09-15 12:57:46 -0500

    Add commented out draft load balancer
    This adds a first attempt at setting up the load
    balancer using terraform.  HItting an error with declaring gce-lb-http.backends.groups[0].group
    
    This passes the self-link, URL to the backend bucket resource, but is getting an error, hence the commenting out of this module right now.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,6 +6,8 @@ variable "google_project_id" {}
 variable "google_project_name" {}
 variable "google_storage_bucket_name" {}
 variable "google_region" { default = "us-central1" }
+# variable "service_port" { default = 80}
+# variable "service_port_name" {default = "http" }
 
 # GLOBAL
 terraform {
@@ -121,5 +123,71 @@ resource "google_storage_bucket_object" "analytics" {
   cache_control = "no-cache, no-store, must-revalidate"
 }
 
-# OUTPUTS
+# https load balancer
+# module "gce-lb-http" {
+#   depends_on = [module.project-services]
 
+#   source  = "GoogleCloudPlatform/lb-http/google"
+#   version = "~> 6.3.0"
+
+#   project     = var.google_project_id
+#   name        = "dot-http-lb"
+#   # target_tags = [module.mig1.target_tags, module.mig2.target_tags]
+
+#   backends = {
+#     default = {
+#       description             = "Load balancer for dot pixel tracking."
+#       protocol                = "HTTP"
+#       port                    = var.service_port
+#       port_name               = var.service_port_name
+#       timeout_sec             = 10
+#       enable_cdn              = true
+#       custom_request_headers  = null
+#       custom_response_headers = null
+#       security_policy         = null
+
+#       connection_draining_timeout_sec = null
+#       session_affinity                = null
+#       affinity_cookie_ttl_sec         = null
+
+#       health_check = {
+#         check_interval_sec  = null
+#         timeout_sec         = null
+#         healthy_threshold   = null
+#         unhealthy_threshold = null
+#         request_path        = "/"
+#         port                = var.service_port
+#         host                = null
+#         logging             = null
+#       }
+
+#       log_config = {
+#         enable      = true
+#         sample_rate = 1.0
+#       }
+
+#       groups = [
+#         {
+#           # Each node pool instance group should be added to the backend.
+#           group                        = google_compute_backend_bucket.dot-bucket-backend.self_link
+#           balancing_mode               = null
+#           capacity_scaler              = null
+#           description                  = null
+#           max_connections              = null
+#           max_connections_per_instance = null
+#           max_connections_per_endpoint = null
+#           max_rate                     = null
+#           max_rate_per_instance        = null
+#           max_rate_per_endpoint        = null
+#           max_utilization              = null
+#         },
+#       ]
+
+#       iap_config = {
+#         enable               = false
+#         oauth2_client_id     = null
+#         oauth2_client_secret = null
+#       }
+#     }
+#   }
+# }
